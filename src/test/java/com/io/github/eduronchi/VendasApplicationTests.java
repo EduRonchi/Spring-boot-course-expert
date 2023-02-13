1e228db2f04b128aeee130c5fa4c65ec3383fa47
Eduardo Ronchi <financaronchi@gmail.com>
2023-02-13 18:00:49 -0300

    Create project

---
+++ src/test/java/com/io/github/eduronchi/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.eduronchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
