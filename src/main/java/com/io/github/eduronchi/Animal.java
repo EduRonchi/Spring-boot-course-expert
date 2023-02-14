8b302550c22a7ab6ea34dcebe180829e34c46a6e
Eduardo Ronchi <financaronchi@gmail.com>
2023-02-14 19:57:58 -0300

    Configurando conexão com banco de dados

--- src/main/java/com/io/github/eduronchi/Animal.java
+++
@@ -1,6 +0,0 @@
-package com.io.github.eduronchi;
-
-
-public interface Animal {
-    void fazerBarulho();
-}
--- src/main/java/com/io/github/eduronchi/VendasApplication.java
+++ src/main/java/com/io/github/eduronchi/VendasApplication.java
@@ -1,33 +1,11 @@
 package com.io.github.eduronchi;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
-	@Value("${application.name}")
-	private String applicationName;
 
-	@Gato
-	private Animal animal;
-
-	@Bean(name = "executarAnimal")
-	public CommandLineRunner executar(){
-		return args -> {
-			this.animal.fazerBarulho();
-		};
-	}
-
-	@GetMapping("/hello")
-	public String helloWorld(){
-		return applicationName;
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
