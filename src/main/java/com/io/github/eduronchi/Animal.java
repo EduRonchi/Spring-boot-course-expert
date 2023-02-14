eff0f98fb04782cac9533aba4850f766572c6008
Eduardo Ronchi <financaronchi@gmail.com>
2023-02-13 23:21:52 -0300

    Criando annotations customizadas de configuração

---
+++ src/main/java/com/io/github/eduronchi/Animal.java
@@ -0,0 +1,6 @@
+package com.io.github.eduronchi;
+
+
+public interface Animal {
+    void fazerBarulho();
+}
--- src/main/java/com/io/github/eduronchi/MinhaConfiguration.java
+++ src/main/java/com/io/github/eduronchi/MinhaConfiguration.java
@@ -2,18 +2,14 @@ package com.io.github.eduronchi;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MinhaConfiguration {
 
     @Bean
-    public CommandLineRunner executar() {
+    public CommandLineRunner executar(){
         return args -> {
             System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
         };
     }
-
 }
