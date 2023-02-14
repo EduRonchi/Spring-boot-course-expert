2a51723395a1b8d9481b3a25e50ecd92ef264d1b
Eduardo Ronchi <financaronchi@gmail.com>
2023-02-13 22:39:16 -0300

    Arquivo application.properties

--- src/main/java/com/io/github/eduronchi/MinhaConfiguration.java
+++ src/main/java/com/io/github/eduronchi/MinhaConfiguration.java
@@ -1,14 +1,8 @@
 package com.io.github.eduronchi;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de vendas";
-    }
 }
