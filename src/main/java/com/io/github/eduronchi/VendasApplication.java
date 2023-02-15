19a43813625fc4dbfb8a53acb05fca5b223a1b83
Eduardo Ronchi <financaronchi@gmail.com>
2023-02-15 19:35:12 -0300

    Persistindo entidades com entityManager

--- src/main/java/com/io/github/eduronchi/VendasApplication.java
+++ src/main/java/com/io/github/eduronchi/VendasApplication.java
@@ -23,29 +23,29 @@ public class VendasApplication {
 			List<Cliente> todosClientes = clientes.obterTodos();
 			todosClientes.forEach(System.out::println);
 
-			System.out.println("Atualizando clientes");
-			todosClientes.forEach(c -> {
-				c.setName(c.getName() + " atualizado.");
-				clientes.atualizar(c);
-			});
+//		System.out.println("Atualizando clientes");
+//		todosClientes.forEach(c -> {
+//			c.setName(c.getName() + " atualizado.");
+//			clientes.atualizar(c);
+//		});
 
-			todosClientes = clientes.obterTodos();
-			todosClientes.forEach(System.out::println);
+//		todosClientes = clientes.obterTodos();
+//		todosClientes.forEach(System.out::println);
 
-			System.out.println("Buscando clientes");
-			clientes.buscarPorName("Cli").forEach(System.out::println);
+//		System.out.println("Buscando clientes");
+//		clientes.buscarPorName("Cli").forEach(System.out::println);
 
 //            System.out.println("deletando clientes");
 //            clientes.obterTodos().forEach(c -> {
 //                clientes.deletar(c);
 //            });
 
-			todosClientes = clientes.obterTodos();
-			if(todosClientes.isEmpty()){
-				System.out.println("Nenhum cliente encontrado.");
-			}else{
-				todosClientes.forEach(System.out::println);
-			}
+//		todosClientes = clientes.obterTodos();
+//		if(todosClientes.isEmpty()){
+//			System.out.println("Nenhum cliente encontrado.");
+//		}else{
+//			todosClientes.forEach(System.out::println);
+//			}
 		};
 	}
 
--- src/main/java/com/io/github/eduronchi/domain/repository/Clientes.java
+++ src/main/java/com/io/github/eduronchi/domain/repository/Clientes.java
@@ -1,10 +1,12 @@
 package com.io.github.eduronchi.domain.repository;
 
 import com.io.github.eduronchi.domain.entity.Cliente;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,15 +15,19 @@ import java.util.List;
 @Repository
 public class Clientes {
 
-    private static String INSERT = "insert into cliente (name) values (?) ";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
     private static String UPDATE = "update cliente set name = ? where id = ? ";
     private static String DELETE = "delete from cliente where id = ? ";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
     public Cliente salvar(Cliente cliente){
-        jdbcTemplate.update( INSERT, new Object[]{cliente.getName()} );
+        entityManager.persist(cliente);
         return cliente;
     }
 
