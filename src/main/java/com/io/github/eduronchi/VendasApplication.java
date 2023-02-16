b133df3cb289d86fb29a3eb59860ac2e3d25594b
Eduardo Ronchi <financaronchi@gmail.com>
2023-02-16 17:29:53 -0300

    Utilizando @query

--- src/main/java/com/io/github/eduronchi/VendasApplication.java
+++ src/main/java/com/io/github/eduronchi/VendasApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class VendasApplication {
 
@@ -18,8 +20,8 @@ public class VendasApplication {
 			clientes.save(new Cliente("Eduardo"));
 			clientes.save(new Cliente("Outro Cliente"));
 
-			boolean existe = clientes.existsByName("Eduardo");
-			System.out.println("existe um cliente com o nome Eduardo ? " + existe);
+			List<Cliente> result = clientes.encontrarPorName("Eduardo");
+			result.forEach(System.out::println);
 
 		};
 	}
--- src/main/java/com/io/github/eduronchi/domain/repository/Clientes.java
+++ src/main/java/com/io/github/eduronchi/domain/repository/Clientes.java
@@ -2,12 +2,20 @@ package com.io.github.eduronchi.domain.repository;
 
 import com.io.github.eduronchi.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface Clientes extends JpaRepository <Cliente, Integer> {
 
-   List<Cliente> findByNameLike(String name);
+   @Query(value = "select * from Cliente c where c.name like '%:name'", nativeQuery = true)
+   List<Cliente> encontrarPorName(@Param("name") String name);
+
+   @Query("delete from Cliente c where c.name =:name")
+   @Modifying
+   void deleteByName(String name);
 
    boolean existsByName(String name);
 
