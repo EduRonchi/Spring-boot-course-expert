414757dd4918b746387f9877c5ed91adff13e335
Eduardo Ronchi <financaronchi@gmail.com>
2023-02-20 23:26:56 -0300

    Criação do Controller e Serviço de Pedidos

---
+++ src/main/java/com/io/github/eduronchi/domain/service/PedidoService.java
@@ -0,0 +1,4 @@
+package com.io.github.eduronchi.domain.service;
+
+public interface PedidoService {
+}
---
+++ src/main/java/com/io/github/eduronchi/rest/controller/PedidoController.java
@@ -0,0 +1,16 @@
+package com.io.github.eduronchi.rest.controller;
+
+import com.io.github.eduronchi.domain.service.PedidoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidoController {
+
+    private PedidoService service;
+
+    public PedidoController(PedidoService service) {
+        this.service = service;
+    }
+}
